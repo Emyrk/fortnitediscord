3f30b1a7851f2df428de3721458ecfaa9009bd08
Emyrk <stevenmasley@gmail.com>
2018-02-27 22:35:41 -0600

    Added arguments

--- matchwatcher/gonode/index.js
+++ matchwatcher/gonode/index.js
@@ -9,7 +9,7 @@ user = process.env.EPIC_USERNAME
 pass = process.env.EPIC_PASSWORD
 
 
-console.log(launcherkey)
+console.log(args[1])
 
 let fortniteAPI = new Fortnite([EPIC_USERNAME, EPIC_PASSWORD, n1, h1]);
 
